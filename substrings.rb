e7d20054efbb9048ba317cfda0baed4ecaab1ce5
t-drape <thebatreview1@gmail.com>
2024-07-16 16:56:10 -0700

    Complete code for one word

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,38 @@
   # Step 1:
   # Get text and dictionary
-
-
-def substrings(text, dictionary)
   # Step 2:
   # Pass text and dictionary to method
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(text, dictionary)
+  substrings_key_element_from_dict_value_one = Hash.new
   # Step 3:
   # Make sure method works for one word
+  word_locase = text.downcase
+  new_dictionary_words_locase = dictionary.map {|word| word.downcase}
+
+
   # Step 3a: 
   # Access every element from dictionary
-  # Step 3b:
-  # If element from dictionary is a substring of word
-  # Step 3ca: 
-  # Add dictionary element to new Hash
-  # Step 3cb:
-  # Set dictionary element value equal to 1
+  for word in new_dictionary_words_locase
+    # Step 3b:
+    # If element from dictionary is a substring of word
+    if word_locase.include? word
+        # Step 3ca: 
+        # Add dictionary element to new Hash
+        # Step 3cb:
+        # Set dictionary element value equal to 1
+      if substrings_key_element_from_dict_value_one[word]
+        substrings_key_element_from_dict_value_one += 1
+      else
+        substrings_key_element_from_dict_value_one[word] = 1
+      end
+    end
+  end
+
   # Step 4:
+  return substrings_key_element_from_dict_value_one
   # Return Hash
   
   # Step 4:
@@ -41,4 +58,7 @@ def substrings(text, dictionary)
   # Step 6:
   # Return new Hash  
 end
+
+
+substrings("below", dictionary)
 
