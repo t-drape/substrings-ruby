6bc31adfe893e73efcd15df91c8108dd089b909b
t-drape <thebatreview1@gmail.com>
2024-07-16 16:39:53 -0700

    Add all pseudocode in comments

---
+++ substrings.rb
@@ -0,0 +1,44 @@
+  # Step 1:
+  # Get text and dictionary
+
+
+def substrings(text, dictionary)
+  # Step 2:
+  # Pass text and dictionary to method
+  # Step 3:
+  # Make sure method works for one word
+  # Step 3a: 
+  # Access every element from dictionary
+  # Step 3b:
+  # If element from dictionary is a substring of word
+  # Step 3ca: 
+  # Add dictionary element to new Hash
+  # Step 3cb:
+  # Set dictionary element value equal to 1
+  # Step 4:
+  # Return Hash
+  
+  # Step 4:
+  # Allow to method to accept multiple words
+  # Step 4a:
+  # Access every element of text
+  # Step 4b: 
+  # For every text element, access every element of dictionary
+  # Step 4c:
+  # If element of dictionary is a substring of element of text
+  # Step 4d:
+  # Add dictionary element to new Hash
+  # Step 4e:
+  # Set value of dictionary element to 1
+  # Step 5:
+  # While accessing every element of text,
+  # Step 5a:
+  # If dictionary element is key in new Hash,
+  # Step 5aa:
+  # Increment value by 1
+  # Step 5ab:
+  # Else, set value equal to 1
+  # Step 6:
+  # Return new Hash  
+end
+
